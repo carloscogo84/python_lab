47642893f9f757f418d78ae8ebb5380ff0010cb5
carloscogo84 <carloscogo84@gmail.com>
2024-06-13 10:27:39 +0100

    introdução a tipos de dados

---
+++ 00 - Fundamentos/aula_002.py
@@ -0,0 +1,23 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+# Usando aspas simples
+print('Este é uma string')
+
+# Usando aspas duplas
+print("Texto")
+
+# Escape
+print("Mateus \"Henrique\"")  # \" - pula o proximo caracter, nao precisa ler
+print("Mateus \nHenrique")
+
+# r
+print(r"Mateus \"Henrique\"")  # expressão regulares
+
+# mais usado
+
+print(1, "Mateus 'Henrique'")
