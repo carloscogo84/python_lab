00b441c3c44799ae0e07bab62ce000faa38cbad4
carloscogo84 <carloscogo84@gmail.com>
2024-06-13 08:10:17 +0100

    comentarios e docstring

---
+++ 00 - Fundamentos/000_aula.py
@@ -0,0 +1,14 @@
+# COMENTÁRIOS
+
+# Comentario de uma linha
+
+# Permite escrever um comentário
+print(123)  # Função que imprime no trminal 123
+
+
+# DOCSTRING
+"""
+Docstring não é um comentário
+Escreva tudo o quer for necessáio do seu código
+
+"""
