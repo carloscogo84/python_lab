eaeb3af92976f135f64fc0a237d1e857fd8da973
carloscogo84 <carloscogo84@gmail.com>
2024-06-13 08:26:16 +0100

    função print

---
+++ 00 - Fundamentos/aula_001.py
@@ -0,0 +1,9 @@
+# FUNÇÃO PRINT
+
+# Neste a caso a função print recebe (argumentos não nomeados)
+print(1)
+print(12, 10)
+
+# Usando um separador
+print(15, 25, sep="-")
+print(30, 40, sep="-", end="\n.")  # \n quebra de linha
