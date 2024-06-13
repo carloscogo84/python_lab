cf842a77d8235628e2cbb568f91c62998087d309
carloscogo84 <carloscogo84@gmail.com>
2024-06-13 20:48:01 +0100

    typecasting

---
+++ 00 - Fundamentos/aula_005.py
@@ -0,0 +1,14 @@
+"""
+conversão de tipos, coerção
+type convertion, typecasting, coercion
+É o ato de converter um tipo em outro tipos imutáveis e primitivos:
+str, int, float, bool
+
+"""
+
+# print("1" + 1)
+print(int("1") + 1)
+print(float("2") + 2)
+print(bool())  # Uma string vazia é considerada falsa
+print(bool(" "))  # Uma string, mesmo com espaço vazio é considerado verdadeiro
+print(str(11) + "b")
