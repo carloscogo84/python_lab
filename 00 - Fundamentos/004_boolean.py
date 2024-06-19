ff7ccdc70ee10b66b0ce60a24e939bfd7d2164cc
carloscogo84 <107104926+carloscogo84@users.noreply.github.com>
2024-06-19 10:01:35 +0100

    Tipo de dado BOOLEAN

--- 00 - Fundamentos/004_boolean.py
+++ 00 - Fundamentos/004_boolean.py
@@ -1,7 +1,7 @@
 # TIPO DE DADOS BOOL (BOOLEAN)
 
 # Ao questionar algo em um programa, só existem duas respostas possíveis:
-# sim (True) ou não (False).
+# sim (True) ou não (False)
 # Existem vários operadores para "questionar". Dentre eles o ==, que é um operador lógico que questiona se um valor é igual a outro
 
 print(type(True))
